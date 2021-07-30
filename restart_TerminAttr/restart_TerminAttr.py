2b68a622964b68a9669b1a6b0a292a1f36ba7f45
philippebureau <philippe.bureau2@gmail.com>
2021-07-30 13:39:55 -0400

    new code

---
+++ restart_TerminAttr/restart_TerminAttr.py
@@ -0,0 +1,14 @@
+import json
+from cvplibrary.auditlogger import alog
+from cvplibrary import Device, CVPGlobalVariables, GlobalVariableNames
+ip = CVPGlobalVariables.getValue(GlobalVariableNames.CVP_IP)
+scriptArgs = CVPGlobalVariables.getValue(GlobalVariableNames.SCRIPT_ARGS)
+
+target = Device(ip)
+cmdList = [ "enable" , "show hostname" ]
+cmdResponse = target.runCmds(cmdList)
+hostname = cmdResponse[1]['response']['hostname']
+
+alog("restarting TerminAttr from script on %s" % (hostname))
+
+target.runCmds(["enable", "configure", "daemon TerminAttr", "shutdown", "no shutdown")])
