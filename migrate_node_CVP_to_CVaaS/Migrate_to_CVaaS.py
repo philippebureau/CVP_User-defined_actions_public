a047184129b6b8d684db3410eb659e681a61292f
philippebureau <philippe.bureau2@gmail.com>
2021-09-14 09:48:57 -0400

    Update Migrate_to_CVaaS.py

--- migrate_node_CVP_to_CVaaS/Migrate_to_CVaaS.py
+++ migrate_node_CVP_to_CVaaS/Migrate_to_CVaaS.py
@@ -11,8 +11,6 @@ target = Device(ip)
 TOKEN = scriptArgs["Token"]
 VRF = scriptArgs["VRF"]
 
-#target.runCmds(["enable", "bash echo timeout 2 '%s' > /tmp/cv-onboarding-token" % (TOKEN), "configure", "daemon TerminAttr", "exec /usr/bin/TerminAttr -cvaddr=apiserver.arista.io:443 -cvcompression=gzip -cvvrf=%s -taillogs -cvauth=token-secure,/tmp/cv-onboarding-token -smashexcludes=ale,flexCounter,hardware,kni,pulse,strata -ingestexclude=/Sysdb/cell/1/agent,/Sysdb/cell/2/agent" % (VRF), "no shutdown"])
-
 # Write an entry to CVP Log
 alog("Migrating node to CVaaS")
 
