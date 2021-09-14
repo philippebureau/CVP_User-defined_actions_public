1427453d88d96b2a509949277242160819a8f8a4
philippebureau <philippe.bureau2@gmail.com>
2021-09-14 09:45:59 -0400

    Update Migrate_to_CVaaS.py

--- migrate_node_CVP_to_CVaaS/Migrate_to_CVaaS.py
+++ migrate_node_CVP_to_CVaaS/Migrate_to_CVaaS.py
@@ -13,9 +13,18 @@ VRF = scriptArgs["VRF"]
 
 #target.runCmds(["enable", "bash echo timeout 2 '%s' > /tmp/cv-onboarding-token" % (TOKEN), "configure", "daemon TerminAttr", "exec /usr/bin/TerminAttr -cvaddr=apiserver.arista.io:443 -cvcompression=gzip -cvvrf=%s -taillogs -cvauth=token-secure,/tmp/cv-onboarding-token -smashexcludes=ale,flexCounter,hardware,kni,pulse,strata -ingestexclude=/Sysdb/cell/1/agent,/Sysdb/cell/2/agent" % (VRF), "no shutdown"])
 
-target.runCmds(["enable", \
- "bash echo '%s' > /tmp/cv-onboarding-token" % (TOKEN), \
- "configure", \
- "daemon TerminAttr", \
- "exec /usr/bin/TerminAttr -cvaddr=apiserver.arista.io:443 -cvcompression=gzip -cvvrf=%s -taillogs -cvauth=token-secure,/tmp/cv-onboarding-token -smashexcludes=ale,flexCounter,hardware,kni,pulse,strata -ingestexclude=/Sysdb/cell/1/agent,/Sysdb/cell/2/agent" % (VRF), \
- "no shutdown"])
+# Write an entry to CVP Log
+alog("Migrating node to CVaaS")
+
+# Migration
+try:
+    target.runCmds(["enable", \
+     "bash echo '%s' > /tmp/cv-onboarding-token" % (TOKEN), \
+     "configure", \
+     "daemon TerminAttr", \
+     "exec /usr/bin/TerminAttr -cvaddr=apiserver.arista.io:443 -cvcompression=gzip -cvvrf=%s -taillogs -cvauth=token-secure,/tmp/cv-onboarding-token -smashexcludes=ale,flexCounter,hardware,kni,pulse,strata -ingestexclude=/Sysdb/cell/1/agent,/Sysdb/cell/2/agent" % (VRF), \
+     "no shutdown"])
+except:
+    alog("Migration failed")
+else:
+    alog("Migration succeded")
